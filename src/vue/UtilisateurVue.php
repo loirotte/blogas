c941d65a49ad05a65e8c7d73eb19f37f2a542b99
Félix Tellitocci <73948501+Magiccarpet00@users.noreply.github.com>
2022-05-13 10:06:10 +0200

    vue change

--- src/vue/UtilisateurVue.php
+++ src/vue/UtilisateurVue.php
@@ -6,12 +6,18 @@ use blogapp\vue\Vue;
 
 class UtilisateurVue extends Vue {
     const NOUVEAU_VUE = 1;
-    
+    const NOUVEAU_CONNEXION = 2;
+
     public function render() {
         switch($this->selecteur) {
-        case self::NOUVEAU_VUE:
-            $content = $this->nouveau();
-            break;
+            case self::NOUVEAU_VUE:
+                $content = $this->nouveau();
+                break;
+//        ça ne marche pas mais ça me semble plutot logique
+            case self::NOUVEAU_CONNEXION:
+                $content = $this->connexion();
+                break;
+
         }
         return $this->userPage($content);
     }
@@ -19,9 +25,35 @@ class UtilisateurVue extends Vue {
     public function nouveau() {
         return <<<YOP
         <form method="post" action="{$this->cont['router']->pathFor('util_cree')}">
-          <input type="text" name="nom">
-          <input type="submit" value="Go go go !">
+        <h1>CREATION</h1>
+          pseudo
+          <input type="text" name="pseudo"><br>
+          nom
+          <input type="text" name="nom"><br>
+          prenom
+          <input type="text" name="prenom"><br>
+          mail
+          <input type="text" name="mail"><br>
+          mot de passe
+          <input type="password" name="mot de passe"><br>
+          <input type="submit" value="Validation">
         </form>
 YOP;
     }
+
+    //Je pense appeler la methode la dans index
+    public function connexion() {
+        return <<<YOP
+        <form method="post" action="{$this->cont['router']->pathFor('util_connexion')}">
+        <h1>CONNEXION</h1>          
+          mail
+          <input type="text" name="mail"><br>
+          mot de passe
+          <input type="password" name="mot de passe"><br>
+          <input type="submit" value="connexion">
+        </form>
+YOP;
+    }
+
+
 }
