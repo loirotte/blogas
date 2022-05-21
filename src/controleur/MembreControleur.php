3bed43436c4b96a1651e129cac9780d7906301cd
TangoAlphaN <parissenathalie@orange.fr>
2022-05-21 18:01:33 +0200

    Update MembreControleur.php

--- src/controleur/MembreControleur.php
+++ src/controleur/MembreControleur.php
@@ -22,5 +22,12 @@ class MembreControleur
     }
 
     //to disconnect
-    
+    public function deconnexion($rq, $rs, $args){
+        if (isset($_COOKIE["membre_authentifier"]))
+        {
+            setcookie("membre");
+        }
+        $this->cont->flash->addMessage('info', "disconnection successfully");
+        return $rs->withRedirect($this->cont->router->pathFor('billet_liste',['numPage' => 1]));
+    }
 }
