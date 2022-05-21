c069012b7049d45e575e2cfd2856897956adebea
TangoAlphaN <parissenathalie@orange.fr>
2022-05-21 16:23:35 +0200

    Update Membre.php

--- src/modele/Membre.php
+++ src/modele/Membre.php
@@ -2,7 +2,7 @@
 
 namespace blogapp\modele;
 
-class Membre extends Illuminate\Database\Eloquent\Model {
+class Membre extends \Illuminate\Database\Eloquent\Model {
     protected $table = 'membres';
     protected $primaryKey = 'id';
     public $pseudo, $nom, $prenom, $mail, $mdp_hash;
