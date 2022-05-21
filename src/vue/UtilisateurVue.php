0204d3ba3e872e7d519480178bc28b5c1f52a3bc
TangoAlphaN <parissenathalie@orange.fr>
2022-05-21 15:34:09 +0200

    Update UtilisateurVue.php

--- src/vue/UtilisateurVue.php
+++ src/vue/UtilisateurVue.php
@@ -20,12 +20,12 @@ class UtilisateurVue extends Vue {
         return <<<YOP
         <form method="post" action="{$this->cont['router']->pathFor('util_cree')}">
             <h1>Inscription d'un nouveau membre</h1>
-            <input type="text" name="pseudo" placeholder="Enter your pseudo" required>
-            <input type="text" name="nom" placeholder="Enter your nom" required>
-            <input type="text" name="prenom" placeholder="Enter your prenom" required>
-            <input type="text" name="mail" placeholder="Enter your mail" required>
-            <input type="text" name="mdp_hash" placeholder="Enter your password" required>
-            <input type="text" name="verification" placeholder="Enter your password again" required>
+            <input type="text" name="pseudo" placeholder="Enter your pseudo" required><br>
+            <input type="text" name="nom" placeholder="Enter your nom" required><br>
+            <input type="text" name="prenom" placeholder="Enter your prenom" required><br>
+            <input type="text" name="mail" placeholder="Enter your mail" required><br>
+            <input type="text" name="mdp_hash" placeholder="Enter your password" required><br>
+            <input type="text" name="verification" placeholder="Enter your password again" required><br>
             <input type="submit" value="Enregistrer">
         </form>
 YOP;
