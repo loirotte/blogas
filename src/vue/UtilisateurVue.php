08a47ae33e7a3f955c2c34af91b1ff64b0572403
TangoAlphaN <parissenathalie@orange.fr>
2022-05-21 15:42:13 +0200

    Update UtilisateurVue.php

--- src/vue/UtilisateurVue.php
+++ src/vue/UtilisateurVue.php
@@ -18,14 +18,14 @@ class UtilisateurVue extends Vue {
 
     public function nouveau() {
         return <<<YOP
-        <form method="post" class="toggle-all" action="{$this->cont['router']->pathFor('util_cree')}">
+        <form method="post" action="{$this->cont['router']->pathFor('util_cree')}">
             <h1>Inscription d'un nouveau membre</h1>
-            <input type="text" name="pseudo" placeholder="Enter your pseudo" required><br>
-            <input type="text" name="nom" placeholder="Enter your nom" required><br>
-            <input type="text" name="prenom" placeholder="Enter your prenom" required><br>
-            <input type="text" name="mail" placeholder="Enter your mail" required><br>
-            <input type="text" name="mdp_hash" placeholder="Enter your password" required><br>
-            <input type="text" name="verification" placeholder="Enter your password again" required><br>
+            <input type="text" class="toggle-all" name="pseudo" placeholder="Enter your pseudo" required><br>
+            <input type="text" class="toggle-all" name="nom" placeholder="Enter your nom" required><br>
+            <input type="text" class="toggle-all" name="prenom" placeholder="Enter your prenom" required><br>
+            <input type="text" class="toggle-all" name="mail" placeholder="Enter your mail" required><br>
+            <input type="text" class="toggle-all" name="mdp_hash" placeholder="Enter your password" required><br>
+            <input type="text" class="toggle-all" name="verification" placeholder="Enter your password again" required><br>
             <input type="submit" value="Enregistrer">
         </form>
 YOP;
