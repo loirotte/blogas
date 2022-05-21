c7af53aa68ca9451264fc7d4d366a12d42753336
TangoAlphaN <parissenathalie@orange.fr>
2022-05-21 18:05:26 +0200

    Update MembreControleur.php

--- src/controleur/MembreControleur.php
+++ src/controleur/MembreControleur.php
@@ -30,4 +30,35 @@ class MembreControleur
         $this->cont->flash->addMessage('info', "disconnected successfully");
         return $rs->withRedirect($this->cont->router->pathFor('billet_liste',['numPage' => 1]));
     }
+
+    //member authentication
+    public function authentifie($rq, $rs, $args) {
+        // Récupération variable POST + nettoyage
+        $email = filter_var($rq->getParsedBodyParam('email'), FILTER_SANITIZE_STRING);
+        $password = filter_var($rq->getParsedBodyParam('password'), FILTER_SANITIZE_STRING);
+
+        $membre = Membre::where('email','=',$email)->first();
+        if ($membre === null)
+        {
+            $this->cont->flash->addMessage('error', "Erreur : email incorrect");
+            return $rs->withRedirect($this->cont->router->pathFor('memb_co'));
+        }
+        else
+        {
+            $hash = $membre->hash;
+            if (password_verify($password, $hash)) {
+                $pseudo=$membre->pseudo;
+                setcookie("membre_authentifier",$pseudo,time()+7*24*3600);
+            }
+            else{
+                $this->cont->flash->addMessage('error', "Erreur : mot de passe incorrect");
+                return $rs->withRedirect($this->cont->router->pathFor('memb_co'));
+            }
+        }
+
+        // Ajout d'un flash
+        $this->cont->flash->addMessage('info', "Utilisateur $pseudo connecté !");
+        // Retour de la réponse avec redirection
+        return $rs->withRedirect($this->cont->router->pathFor('billet_liste',['numPage' =>1]));
+    }
 }
