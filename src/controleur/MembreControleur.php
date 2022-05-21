51414ac441d30c736cd3a9ef0394671486d9f145
TangoAlphaN <parissenathalie@orange.fr>
2022-05-21 17:51:52 +0200

    Update MembreControleur.php

--- src/controleur/MembreControleur.php
+++ src/controleur/MembreControleur.php
@@ -1,8 +1,26 @@
 <?php
 
 namespace blogapp\controleur;
+use blogapp\modele\Membre;
+use blogapp\vue\MembreVue;
 
 class MembreControleur
 {
+    private $cont;
 
+    public function __construct($conteneur)
+    {
+        $this->cont = $conteneur;
+    }
+
+    //accéder à la page de connexion
+    public function connexion($rq, $rs, $args)
+    {
+        $bl = new MembreVue($this->cont, null, MembreVue::NOUVEAU_VUE);
+        $rs->getBody()->write($bl->render());
+        return $rs;
+    }
+
+    //to disconnect
+    
 }
