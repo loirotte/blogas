41f470f0f52bdd32398370541458fcdcaf3706d8
TangoAlphaN <parissenathalie@orange.fr>
2022-05-21 15:45:24 +0200

    Update UtilisateurVue.php

--- src/vue/UtilisateurVue.php
+++ src/vue/UtilisateurVue.php
@@ -20,15 +20,15 @@ class UtilisateurVue extends Vue {
         return <<<YOP
         <form method="post" action="{$this->cont['router']->pathFor('util_cree')}">
             <h1>Inscription d'un nouveau membre</h1>
-            <div class="form-group">
-            <input type="text" class="toggle-all" name="pseudo" placeholder="Enter your pseudo" required><br>
-            <input type="text" class="toggle-all" name="nom" placeholder="Enter your nom" required><br>
-            <input type="text" class="toggle-all" name="prenom" placeholder="Enter your prenom" required><br>
-            <input type="text" class="toggle-all" name="mail" placeholder="Enter your mail" required><br>
-            <input type="text" class="toggle-all" name="mdp_hash" placeholder="Enter your password" required><br>
-            <input type="text" class="toggle-all" name="verification" placeholder="Enter your password again" required><br>
-            <input type="submit" value="Enregistrer">
-            </div>
+                <div class="form-group">
+                    <input type="text" class="toggle-all" name="pseudo" placeholder="Enter your pseudo" required><br>
+                    <input type="text" class="toggle-all" name="nom" placeholder="Enter your nom" required><br>
+                    <input type="text" class="toggle-all" name="prenom" placeholder="Enter your prenom" required><br>
+                    <input type="text" class="toggle-all" name="mail" placeholder="Enter your mail" required><br>
+                    <input type="text" class="toggle-all" name="mdp_hash" placeholder="Enter your password" required><br>
+                    <input type="text" class="toggle-all" name="verification" placeholder="Enter your password again" required><br>
+                    <input type="submit" value="Enregistrer">
+                </div>
         </form>
 YOP;
     }
