c389b1d47d7e017b78b9481ab87d55808a6fdfb9
TangoAlphaN <parissenathalie@orange.fr>
2022-05-21 18:19:29 +0200

    Update UtilisateurVue.php

--- src/vue/UtilisateurVue.php
+++ src/vue/UtilisateurVue.php
@@ -22,9 +22,9 @@ class UtilisateurVue extends Vue {
             <h1>Inscription d'un nouveau membre</h1>
                 <div class="form-group">
                     <input type="text" class="toggle-all" name="pseudo" placeholder="Enter your pseudo" required><br>
-                    <input type="text" class="toggle-all" name="nom" placeholder="Enter your nom" required><br>
-                    <input type="text" class="toggle-all" name="prenom" placeholder="Enter your prenom" required><br>
-                    <input type="text" class="toggle-all" name="mail" placeholder="Enter your mail" required><br>
+                    <input type="text" class="toggle-all" name="nom" placeholder="Enter your lastname" required><br>
+                    <input type="text" class="toggle-all" name="prenom" placeholder="Enter your firstname" required><br>
+                    <input type="text" class="toggle-all" name="mail" placeholder="Enter your email" required><br>
                     <input type="text" class="toggle-all" name="mdp_hash" placeholder="Enter your password" required><br>
                     <input type="text" class="toggle-all" name="verification" placeholder="Enter your password again" required><br>
                     <button type="submit" value="Enregistrer"/>
