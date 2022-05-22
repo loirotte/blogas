96c86b1e6b1c55d59cac25c8a3d61e0c841896ad
TangoAlphaN <parissenathalie@orange.fr>
2022-05-22 15:04:39 +0200

    Update UtilisateurVue.php

--- src/vue/UtilisateurVue.php
+++ src/vue/UtilisateurVue.php
@@ -7,16 +7,16 @@ use blogapp\vue\Vue;
 class UtilisateurVue extends Vue {
     const NOUVEAU_VUE = 1;
     
-    public function render() {
-        switch($this->selecteur) {
-        case self::NOUVEAU_VUE:
+    public function render(): string
+    {
+        if ($this->selecteur == self::NOUVEAU_VUE) {
             $content = $this->nouveau();
-            break;
         }
         return $this->userPage($content);
     }
 
-    public function nouveau() {
+    public function nouveau(): string
+    {
         return <<<YOP
         <form method="post" action="{$this->cont['router']->pathFor('util_cree')}">
             <h1>Inscription d'un nouveau membre</h1>
