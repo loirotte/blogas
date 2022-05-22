f117dabb84e817af0eba3b724abf8fe240ddd8f8
TangoAlphaN <parissenathalie@orange.fr>
2022-05-22 14:58:55 +0200

    Update newBillVue.php

--- src/vue/newBillVue.php
+++ src/vue/newBillVue.php
@@ -1,8 +1,40 @@
 <?php
 
 namespace blogapp\vue;
+use blogapp\vue\vue;
 
 class newBillVue
 {
+    const NOUVEAU_VUE = 1;
+
+    public function render() {
+        switch($this->selecteur){
+            case self::NOUVEAU_VUE:
+                $content = $this->nouveau();
+                break;
+        }
+        return $this->userPage($content);
+    }
+
+    public function nouveau() {
+        return <<<YOP
+            <form method="post" action="{$this->cont['router']->pathFor('bill_cree')}">
+            <h1>New Bill</h1>
+            <input type="text" name="title" class="toggle-all" placeholder="Bill Title" required><br>
+            <textarea name="body" class="txtarea" placeholder="Bill text/content" required/><br>
+            <p>pick your article category :</p><br>
+            <div>
+                <input type="radio" name="category" value="Sport">
+                <input type="radio" name="category" value="Cinema">
+                <input type="radio" name="category" value="Music">
+                <input type="radio" name="category" value="TV">
+                <input type="radio" name="category" value="Other">
+            </div>
+            <input type="submit" name="Submit">
+            </form>
+YOP;
 
 }
+}
+
+?>
