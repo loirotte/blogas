6e20d11a56f227f1a9abe567756ab230002ac724
TangoAlphaN <parissenathalie@orange.fr>
2022-05-22 15:14:17 +0200

    Update newBillControleur.php

--- src/controleur/newBillControleur.php
+++ src/controleur/newBillControleur.php
@@ -19,4 +19,29 @@ class newBillControleur
         $rs->getBody()->write($bill->render());
         return $rs;
     }
+
+    public function saisie($rq, $rs, $args)
+    {
+        $title = filter_var($rq->getParsedBodyParam('title'), FILTER_SANITIZE_STRING);
+        $body = filter_var($rq->getParsedBodyParam('body'), FILTER_SANITIZE_STRING);
+        $category = filter_var($rq->getParsedBodyParam('category'), FILTER_SANITIZE_STRING);
+        $date = date("Y-m-d");
+
+        $categories = Categorie::get();
+        foreach ($categories as $categ) {
+            if ($categ->titre === $category) {
+                $cat_id = $categ->id;
+            }
+        }
+
+        //insert into database
+        $billet = new Billet();
+        $billet->title = $title;
+        $billet->body = $body;
+        $billet->date = $date;
+        $billet->category = $cat_id;
+        $billet->save();
+
+        $this->cont->flash->addMessage('info', "Bill added successfully");
+        return $rs->withRedirect($this->cont->router->pathFor('billet_liste',['numPage' =>1]));
 }
